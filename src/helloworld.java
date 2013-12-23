9e26c13dc3e5a11dafe6401cfc82f399a730ad47
michael alleyene <windfish93@gmail.com>
2013-12-23 03:33:00 -0500

    Second Commit

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,5 @@
 class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello Squirrel"); // Display the string.
     }
 }
